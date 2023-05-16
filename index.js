805a74052c0a4af95f1e9167c74dbd48cdec98e4
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-05-16 15:14:02 -0500

    completed assignemnt

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 // Write your solution in this file!
-var customerName = 'bob'; 
+var customerName = 'bob'
+//Declare a variable in global scope called customerName using the var keyword and assign it the value 'bob'.
+const leastFavoriteCustomer = 'mike'
 
 function upperCaseCustomerName() {
-    customerName = customerName.toUpperCase();
-}
-function setBestCustomer () {
-    bestCustomer ='not bob';
+  customerName = customerName.toUpperCase()
+  //Write a function that accesses that global customerName variable, and uppercases it.
+  
+  //notice that there is no return statement
+  //if you try to use `return customerName.toUpperCase()`, it will return 'BOB', but will not redefine the variable!
 }
 
-function overwriteBestCustomer(){
-    bestCustomer ='maybe bob';
+function setBestCustomer() {
+  bestCustomer = 'not bob'
 }
-const leastFavoriteCustomer = 'probably bob'
-function changeLeastFavoriteCustomer(){
-    leastFavoriteCustomer = 'definitely bob';
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'billy'
+  console.log(leastFavoriteCustomer)
 }
